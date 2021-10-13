8ea14c22591dd3f4be4434a906332596603d32f7
dvdsphp <david.sopheap@yahoo.ca>
2021-10-12 23:24:15 -0700

    cleaned up UI, cleaned up signatures, refactoring, tests, some signatures, and cleaning up UI to pull more functionality into Players class remains

---
+++ src/main/model/BasePiece.java
@@ -0,0 +1,36 @@
+package model;
+
+// Represents a chess piece that has a colour.
+public abstract class BasePiece {
+    protected String colour;
+
+    public BasePiece(String colour) {
+        this.colour = colour;
+    }
+
+    // Methods:
+    // ===================================================
+    // EFFECTS: prints the first letter of the piece's name as a capital letter.
+    protected abstract String printPiece();
+
+    // EFFECTS: prints the first letter of the piece's colour as a lowercase letter.
+    public String printColourOneCharacter() {
+        if (colour.equals("white")) {
+            return ("w");
+        } else {
+            return ("b");
+        }
+    }
+
+    // Getters:
+    // ===================================================
+    public String getColour() {
+        return colour;
+    }
+
+    // Setters:
+    // ===================================================
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
